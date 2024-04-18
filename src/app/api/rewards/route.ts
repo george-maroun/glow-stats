d7861a83ada30e918cb357219f28cc2db4f8aebe
George M <gsmaroun@gmail.com>
2024-04-18 16:36:38 -0400

    Remove wagmi code

--- src/app/api/rewards/route.ts
+++ src/app/api/rewards/route.ts
@@ -1,38 +1,38 @@
-import { NextResponse } from 'next/server';
-export const revalidate = 1000;
+// import { NextResponse } from 'next/server';
+// export const revalidate = 1000;
 
-import { createPublicClient, http } from 'viem';
-import { mainnet } from '@wagmi/core/chains'
-import {
-  getWeeklyRewardsForWeeksMulticall,
-} from '../../../../multicalls/view/getWeeklyRewardsForWeeksMulticall';
-import getWeeksSinceStart from '../../../../lib/utils/currentWeekHelper';
+// import { createPublicClient, http } from 'viem';
+// import { mainnet } from '@wagmi/core/chains'
+// import {
+//   getWeeklyRewardsForWeeksMulticall,
+// } from '../../../../multicalls/view/getWeeklyRewardsForWeeksMulticall';
+// import getWeeksSinceStart from '../../../../lib/utils/currentWeekHelper';
 
-const getRewards = async () => {
-  const transport = http(process.env.INFURA_URL!)
-  const viemClient = createPublicClient({
-    transport: transport,
-    chain: mainnet, //need mainnet import for the multicall
-  })
+// const getRewards = async () => {
+//   const transport = http(process.env.INFURA_URL!)
+//   const viemClient = createPublicClient({
+//     transport: transport,
+//     chain: mainnet, //need mainnet import for the multicall
+//   })
 
-  const currentWeek = getWeeksSinceStart()
-  const lastWeekToFetch = currentWeek + 208
-  const rewards = await getWeeklyRewardsForWeeksMulticall({
-    client: viemClient,
-    weekStart: currentWeek,
-    weekEnd: lastWeekToFetch,
-  })
-  return { rewards }
-}
+//   const currentWeek = getWeeksSinceStart()
+//   const lastWeekToFetch = currentWeek + 208
+//   const rewards = await getWeeklyRewardsForWeeksMulticall({
+//     client: viemClient,
+//     weekStart: currentWeek,
+//     weekEnd: lastWeekToFetch,
+//   })
+//   return { rewards }
+// }
 
-export async function GET() {
-  const rewards = await getRewards();
-  try {
-    return NextResponse.json(rewards);
-  }
-  catch (error) {
-    console.error('Error fetching data:', error);
+// export async function GET() {
+//   const rewards = await getRewards();
+//   try {
+//     return NextResponse.json(rewards);
+//   }
+//   catch (error) {
+//     console.error('Error fetching data:', error);
 
-  }
-  return NextResponse.json({ rewards: undefined });
-}
+//   }
+//   return NextResponse.json({ rewards: undefined });
+// }
