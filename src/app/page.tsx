8a70b9f48cde99f0a36caebae3994906aab9720e
George M <gsmaroun@gmail.com>
2024-07-05 22:08:35 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchTime = async () => {
-      const res = await fetch('/api/testRoute', { cache: 'no-store' });
+      const res = await fetch('/api/testRoute');
       const data = await res.json();
       setTime(data.time);
     }
