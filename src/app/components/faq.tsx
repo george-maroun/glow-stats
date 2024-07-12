7fa93216c1f767f5d802d863f512b9a7022ca03a
George <93730150+george-maroun@users.noreply.github.com>
2024-07-12 11:20:36 -0400

    change font size in QA

--- src/app/components/faq.tsx
+++ src/app/components/faq.tsx
@@ -66,7 +66,7 @@ const FAQ: React.FC = () => {
               className="flex justify-between items-center cursor-pointer"
               onClick={() => toggleQuestion(index)}
             >
-              <h3 className="text-2xl font-lg">{item.question}</h3>
+              <h3 className="lg:text-2xl text-xl font-lg">{item.question}</h3>
               <button className="text-2xl focus:outline-none">
                 {openQuestions.has(index) ? '−' : '+'}
               </button>
@@ -76,7 +76,7 @@ const FAQ: React.FC = () => {
                 openQuestions.has(index) ? 'max-h-96' : 'max-h-0'
               }`}
             >
-              <p className="text-xl leading-relaxed opacity-80">{item.answer}</p>
+              <p className="lg:text-xl text-lg leading-relaxed opacity-80">{item.answer}</p>
             </div>
           </div>
         ))}
