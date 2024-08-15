67c65405e506e2cbe4824309c61f126b4ae55f7a
George Maroun <gsmaroun@gmail.com>
2024-08-14 21:23:11 -0400

    Increase time to revalidate

--- src/app/api/carbonCreditPrice/route.ts
+++ src/app/api/carbonCreditPrice/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-export const revalidate = 60;
+export const revalidate = 0;
 
 export async function GET() {
   try {
--- src/app/api/locationData/route.ts
+++ src/app/api/locationData/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../../lib/prisma';
 import getPlaceName from '../../../../lib/utils/getPlaceNameHelper'
-export const revalidate = 60;
+export const revalidate = 6000;
 
 // type Param = number | string;
 
